0fb9cf58c231557a1f09030f867ec6074df58a85
franklinnwren <45508028+franklinnwren@users.noreply.github.com>
2022-06-05 21:54:02 -0700

    Add files via upload

--- Course_Project/Group13_Dissimilar_Nodes_Improves_Graph_Active_Learning/model_variants/SGC/citation.py
+++ Course_Project/Group13_Dissimilar_Nodes_Improves_Graph_Active_Learning/model_variants/SGC/citation.py
@@ -16,7 +16,7 @@ from scipy import stats
 from sklearn.metrics import f1_score
 from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import euclidean_distances
-
+import sklearn
 import sys
 
 seed = 123
@@ -31,11 +31,18 @@ print(torch.cuda.current_device())
 if args.dataset == 'cora':
     NL = 140
     NCL = 7
-    basef = 0.995
+    if sys.argv[2] != 'combined':
+      basef = 0.99
+    else:
+      basef = 0.95
+    
 elif args.dataset == 'citeseer':
     NL = 120
     NCL = 6
-    basef = 0.9
+    if sys.argv[2] != 'combined':
+      basef = 0.9
+    else:
+      basef = 0.85
 else:
     raise NotImplementedError('Invalid dataset')
 
@@ -85,17 +92,23 @@ normcen = np.loadtxt("res/"+ args.dataset +"/graphcentrality/normcen")
 cenperc = np.asarray([perc(normcen,i) for i in range(len(normcen))])
 
 def train_AL(model,
-             features, labels, idx_train, idx_val, idx_test,
+             features, labels, idx_train, idx_val, idx_test, message_passing, raw_features,
              epochs=args.epochs, weight_decay=args.weight_decay,
              lr=args.lr):
     optimizer = optim.Adam(model.parameters(), lr=lr,
                            weight_decay=weight_decay)
     t = perf_counter()
     softmax = torch.nn.Softmax(dim=1)
+    message_passing = message_passing.detach().to('cpu').to_dense().numpy()
     for epoch in range(epochs):
         gamma = np.random.beta(1, 1.005 - basef ** epoch)
-        alpha = beta = (1 - gamma) / 2
-
+        if sys.argv[2] == 'baseline':
+          alpha = beta = delta = epsilon = (1-gamma)/2
+        elif sys.argv[2] == 'combined':
+          alpha = beta = delta = epsilon = (1-gamma)/4
+        else:
+          alpha = beta = delta = epsilon = (1-gamma)/3
+        
         model.train()
         optimizer.zero_grad()
         output = model(features)
@@ -105,6 +118,30 @@ def train_AL(model,
 
         embs = softmax(output).cpu().detach().numpy()
         if len(idx_train) < NL:
+            curr_features = raw_features[idx_train, :]
+            curr_features = sklearn.preprocessing.normalize(curr_features)
+            raw_features = sklearn.preprocessing.normalize(raw_features)
+            similarity = []
+            for i in curr_features:
+              similarity.append(np.dot(raw_features, np.squeeze([i])))
+            similarity = np.squeeze(np.array(similarity))
+            max_similarity = np.max(similarity, axis=0)
+            simprec = np.asarray([percd(max_similarity,i) for i in range(len(max_similarity))])
+
+            curr_embeddings = output.to('cpu').detach().numpy()[idx_train, :]
+            curr_embeddings = sklearn.preprocessing.normalize(curr_embeddings)
+            raw_embeddings = sklearn.preprocessing.normalize(output.to('cpu').detach().numpy())
+            em_similarity = []
+            for i in curr_embeddings:
+              em_similarity.append(np.dot(raw_embeddings, np.squeeze([i])))
+            em_similarity = np.squeeze(np.array(em_similarity))
+            max_em_similarity = np.max(em_similarity, axis=0)
+            em_simprec = np.asarray([percd(max_em_similarity,i) for i in range(len(max_em_similarity))])
+
+            connectivity = message_passing[idx_train, :]
+            max_connectivity = np.squeeze(np.array(np.max(connectivity, axis=0)))
+            connprec = np.asarray([percd(max_connectivity,i) for i in range(len(max_connectivity))])
+
             entropy = sc.stats.entropy(embs.T)
             # entropy[train_mask+val_mask+test_mask]=-100
             entrperc = np.asarray([perc(entropy, i) for i in range(len(entropy))])
@@ -112,7 +149,21 @@ def train_AL(model,
             ed = euclidean_distances(embs, kmeans.cluster_centers_)
             ed_score = np.min(ed, axis=1)
             edprec = np.asarray([percd(ed_score, i) for i in range(len(ed_score))])
-            finalweight = alpha * entrperc + beta * edprec + gamma * cenperc
+            if sys.argv[2] == 'baseline':
+              finalweight = alpha*entrperc + beta*edprec + gamma*cenperc
+              print("entropy weight: ", alpha, " density weight: ", beta, "centrality weight: ", gamma)
+            elif sys.argv[2] == 'f_similarity':
+              finalweight = alpha*entrperc + beta*edprec + gamma*cenperc + delta*simprec
+              print("entropy weight: ", alpha, " density weight: ", beta, " centrality weight: ", gamma, " feature similarity weight: ", delta)
+            elif sys.argv[2] == 's_similarity':
+              finalweight = alpha*entrperc + beta*edprec + gamma*cenperc + delta*connprec
+              print("entropy weight: ", alpha, " density weight: ", beta, " centrality weight: ", gamma, " structural similarity weight: ", delta)
+            elif sys.argv[2] == 'e_similarity':
+              finalweight = alpha*entrperc + beta*edprec + gamma*cenperc + delta*em_simprec
+              print("entropy weight: ", alpha, " density weight: ", beta, " centrality weight: ", gamma, " embedding similarity weight: ", delta)
+            else:
+              finalweight = alpha*entrperc + beta*edprec + gamma*cenperc + delta*connprec + epsilon*simprec
+              print("entropy weight: ", alpha, " density weight: ", beta, " centrality weight: ", gamma, " stuructural similarity weight: ", delta, " feature similarity weight: ", epsilon)
             finalweight[idx_train] = -100
             finalweight[idx_test] = -100
             finalweight[idx_val] = -100
@@ -163,7 +214,11 @@ def one_pass(iter):
 
     adj, features, labels, idx_train, idx_val, idx_test = \
         load_citation(args.dataset, args.normalization, args.cuda, iter=iter)
-
+    message_passing = adj * adj
+  
+    # Some preprocessing
+    raw_features = features.to('cpu').numpy()
+    print(raw_features.shape)
     model = get_model(args.model, features.size(1), labels.max().item()+1, args.hidden, args.dropout, args.cuda)
 
     if args.model == "SGC":
@@ -173,15 +228,17 @@ def one_pass(iter):
     idx_train = idx_train.tolist()
     idx_val = idx_val.tolist()
     idx_test = idx_test.tolist()
-    _, _, mac, mic = train_AL(model, features, labels, idx_train, idx_val, idx_test)
+    _, _, mac, mic = train_AL(model, features, labels, idx_train, idx_val, idx_test, message_passing, raw_features)
     return mac, mic
 
 
 if __name__ == "__main__":
     mac_list, mic_list = [], []
-    for i in range(5):
+    for i in range(11):
         mac, mic = one_pass(i)
         mac_list.append(mac)
         mic_list.append(mic)
-    print("Average of macrof1 is {}".format(sum(mac_list) / 5))
-    print("Average of microf1 is {}".format(sum(mic_list) / 5))
+    print(mac_list)
+    print(mic_list)
+    print("Average of macrof1 is {}".format(sum(mac_list) / 11))
+    print("Average of microf1 is {}".format(sum(mic_list) / 11))
